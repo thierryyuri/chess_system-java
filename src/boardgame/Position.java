08ca15e9130d36b0d64e81187a0145b06ba78673
Thierry yuri de oliveira simao <thierry.yurii10@gmail.com>
2024-03-25 13:40:31 -0300

    chesss complete

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,29 +1,37 @@
 package boardgame;
 
 public class Position {
-      private int row;
-      private int column;
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
 	public int getRow() {
 		return row;
 	}
+
 	public void setRow(int row) {
 		this.row = row;
 	}
-	public int getColum() {
+
+	public int getColumn() {
 		return column;
 	}
-	public void setColum(int colum) {
-		this.column = colum;
+
+	public void setColumn(int column) {
+		this.column = column;
 	}
-	public Position(int row, int colum) {
-		super();
+	
+	public void setValues(int row, int column) {
 		this.row = row;
-		this.column = colum;
+		this.column = column;
 	}
 	
 	@Override
 	public String toString() {
 		return row + ", " + column;
-		
 	}
 }
